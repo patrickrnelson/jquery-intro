c205cadbae76449e2ab94c3fe25d4b14248a9909
patrickrnelson <patrickrnelson1@gmail.com>
2021-01-28 16:24:48 -0600

    important notes on jquery event

--- awesomeScript.js
+++ awesomeScript.js
@@ -19,4 +19,42 @@ function onReady() {
   const h1Text = h1.text();
 
   const loudText = h1Text.toUpperCase();
+
+  $('#submit-button').on('click', addHarmonica);
+  // same as...
+  //$(document).on('click', '#submit-button', addHarmonica)
+
+  // the initial thing you target has to be a parent
+  $(document).on('click', '.delete-button', deleteMe);
+  // could also be $('#harmonicaList').on('click', '.delete-button', deleteMe);
+}
+
+function deleteMe() {
+  console.log('Delete!!');
+  // This will remove all items that have this class
+  // $('.harmonica-item').remove();
+
+  // $(this) targets the button
+  let thisThing = $(this);
+  thisThing.parent().remove();
+  //thisThing.remove() only removes the button
+  //thisThing.parent() is the <li>
+}
+
+// event will give us the ability to call the function event.preventDefault()
+function addHarmonica(event) {
+  event.preventDefault();
+  // what we do when we submit
+  console.log('clicked the button!');
+
+  // grab data from from
+  let brand = $('#brand').val();
+  console.log(brand);
+
+  // push form data to the DOM
+  $('#harmonicaList').append(`
+  <li class="harmonica-item">
+    Brand is: ${brand}
+    <button class="delete-button">Delete</button>
+  </li>`);
 }
