87b08749c5474661edfbf368c2c4e319f9f96e01
patrickrnelson <patrickrnelson1@gmail.com>
2021-01-28 13:51:50 -0600

    just jquery tings

--- awesomeScript.js
+++ awesomeScript.js
@@ -4,4 +4,15 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log('jq jq jq');
+
+  const h1 = $('h1');
+  console.log('h1', h1);
+
+  const h2 = $('h2');
+
+  h2.css('background-color', 'blue');
+
+  (greenBackground = 'background-color'), 'green';
+
+  h1.addClass('froggy');
 }
